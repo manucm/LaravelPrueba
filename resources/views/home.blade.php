a8b0935bf5c667a759caa843da317b2a38d10ae8
Manuel Contreras Morillo <manuel.contreras.morillo@gmail.com>
2016-02-13 09:51:10 +0100

    empezar a trabajar con nuestro css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,22 @@
 @section('content')
 
 <h1>Estos son nuestros posts:</h1>
-    <ul>
+<hr>
+    <ul class="list-unstyled">
     @foreach($posts as $post)
 
         <li>
-            <a href="{{  route('post_show_path', $post->id) }}">{{ $post->title }}</a>
-            - {{ $post->user->name }}
+            <p class="lead">
+
+              <a href="{{  route('post_show_path', $post->id) }}">{{ $post->title }}</a>
+              <br>
+              author: {{ $post->user->name }}
+              <br>
+              creado: {{ $post->created_at }}
+
+              <hr>
+            </p>
+
         </li>
 
     @endforeach
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>PlatziPHP y Laravel</title>
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}" media="screen" title="no title" charset="utf-8">
   </head>
   <body>
-    @yield('content')
+        <div class="container">
+              @yield('content')
+        </div>
+
   </body>
 </html>
